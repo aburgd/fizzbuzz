afadc2cb26d33bb15d324e1579b9498769016a66
Alec Burgdorf <aeburgd@gmail.com>
2016-08-07 09:25:32 -0500

    add fizzbuzz.go

---
+++ fizzbuzz.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+  i := 1
+  for i <= 100 {
+    if i % 15 == 0 {
+      fmt.Println("FizzBuzz")
+    } else if i % 3 == 0 {
+      fmt.Println("Fizz")
+    } else if i % 5 == 0 {
+      fmt.Println("Buzz")
+    } else if i % 7 == 0 {
+      fmt.Println("Woof")
+    } else {
+      fmt.Println(i)
+    }
+    i++
+  }
+}
